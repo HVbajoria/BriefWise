b6169567aca297df3a924b924393f7d46b1b8675
Harshavardhan Bajoria <62978274+HVbajoria@users.noreply.github.com>
2023-06-24 13:18:07 +0530

    fixed bug

--- summarizer1.py
+++ summarizer1.py
@@ -12,7 +12,7 @@ def summarizer(text, tokenizer=nlp, max_sent_in_summary=3):
     
     sentence_organizer = {k:v for v,k in enumerate(sentences)}
     
-    vectorizer = TfidfVectorizer(min_df=2, max_dif = 6, 
+    vectorizer = TfidfVectorizer(min_df=2, max_dif = 2, 
                                         max_features=None, 
                                         strip_accents='unicode', 
                                         analyzer='word',
